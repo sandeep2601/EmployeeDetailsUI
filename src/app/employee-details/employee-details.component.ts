2925901d27857f6e512b68144ccfc3972f78731e
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-14 11:46:30 +0530

    Added the delete functionality

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model';
 
 @Component({
   selector: 'app-employee-details',
@@ -10,8 +11,28 @@ export class EmployeeDetailsComponent {
   constructor(public empService: EmployeeService) { }
 
   ngOnInit() {
+    this.getEmployees();
+  }
+
+  getEmployees() {
     this.empService.getEmployees().subscribe(data => {
       this.empService.listEmployees = data;
     });
   }
+
+  editEmployee(seledtedEmployee: Employee) {
+    this.empService.employeeData = seledtedEmployee;
+  }
+
+  deleteEmployee(id: number) {
+    if (confirm('Are you really want to delete this record?')) {
+      this.empService.deleteEmployee(id).subscribe(data => {
+        console.log('Record Deleted...');
+        this.getEmployees();
+      }, err => {
+        console.log('Error: Record not deleted...');
+      })
+    }
+  }
+  
 }
